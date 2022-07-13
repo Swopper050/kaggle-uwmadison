Fix bug in dataset and added softmax for predictions

test_segmentation_model.py
@@ -2,13 +2,14 @@ import argparse
 
 import torch
 import torch.nn.functional as F
-from uwm.segmentation_model import SegmentationDataset, SegmentationUNet
+from uwm.dataset import SegmentationDatamodule
+from uwm.segmentation_model import SegmentationUNet
 from uwm.visualization import plot_prediction
 
 
 def main(args):
-    dataset = SegmentationDataset(batch_size=1)
-    model = SegmentationUNet(n_channels=1, n_classes=4, u_depth=2)
+    dataset = SegmentationDatamodule(batch_size=1)
+    model = SegmentationUNet(n_channels=1, n_classes=4, u_depth=3, start_channels=16)
     model.load_state_dict(torch.load("./models/segmentation_model.pt"))
 
     dataset.setup()

train_present_model.py
@@ -4,17 +4,18 @@ import sys
 
 import pytorch_lightning as pl
 import torch
-from uwm.present_model import PresentDataset, PresentPredictor
+from uwm.dataset import PresentDatamodule
+from uwm.present_model import PresentPredictor
 
 
 def main(args):
-    dataset = PresentDataset(batch_size=args.batch_size)
+    dataset = PresentDatamodule(batch_size=args.batch_size)
     model = PresentPredictor()
 
     trainer = pl.Trainer(
-        gpus=-1,
-        auto_select_gpus=True,
-        enable_progress_bar=False,
+        devices="auto",
+        accelerator="auto",
+        enable_progress_bar=True,
         enable_model_summary=False,
         enable_checkpointing=False,
         max_epochs=args.epochs,
@@ -22,7 +23,7 @@ def main(args):
     )
 
     trainer.fit(model, dataset)
-    torch.save(model.state_dict(), "present_model.pt")
+    torch.save(model.state_dict(), "./present_model.pt")
 
 
 if __name__ == "__main__":

uwm/constants.py
@@ -4,10 +4,7 @@ DATA_DIR = "./input/uw-madison-gi-tract-image-segmentation"
 """ Directory where the dataset for image segmentation is stored. """
 """ Base directory where the data is. """
 
-SEGMENTATION_DATASET_DIR = "./segmentation_dataset"
-""" Directory where the dataset for image segmentation is stored. """
-
-PRESENT_DATASET_DIR = "./present_dataset"
-""" Directory where the dataset for 'present intestines' is stored. """
+PREDICTION_THRESHOLD = 0.5
+""" Threshold for pixel probabilities to be classified as the class. """
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")

uwm/dataset.py
@@ -13,7 +13,7 @@ MAX_CPUS = 8
 """Maximum number of CPU cores to use for loading data examples. """
 
 
-class FileDataset(Dataset):
+class SegmentationFileDataset(Dataset):
     """
     Extended from the torch.utils.data.Dataset, this class can be used in combination
     with a Dataloader to lazily load the dataset using files as examples.
@@ -29,11 +29,11 @@ class FileDataset(Dataset):
         )
         self.transform = transforms.Compose(
             [
-                # Shape (266, 266) because it is most representative
+                # Shape (266, 266) because it is most representative of the dataset.
                 transforms.RandomCrop((266, 266), pad_if_needed=True),
-                transforms.RandomRotation(45),
+                transforms.RandomRotation(15),
                 transforms.RandomVerticalFlip(p=0.5),
-                transforms.RandomPerspective(distortion_scale=0.5, p=0.3),
+                transforms.RandomPerspective(distortion_scale=0.1, p=0.1),
             ]
         )
 
@@ -62,24 +62,130 @@ class FileDataset(Dataset):
         stomach_mask = get_mask_from_rle(sample["stomach"], img.shape)
         masks = np.stack([large_bowel_mask, small_bowel_mask, stomach_mask], axis=0)
 
-        masks_tensor = self.transform(torch.from_numpy(masks).float())
-        background_mask = masks_tensor.sum(axis=0) == 0
-        target_tensor = torch.cat((masks_tensor, background_mask.unsqueeze(0)), dim=0)
+        collection = torch.cat((input_tensor, torch.from_numpy(masks).float()), dim=0)
+        transformed_collection = self.transform(collection)
+        input_tensor = transformed_collection[:1]
+        masks = transformed_collection[1:]
 
-        return self.transform(input_tensor), target_tensor
+        # Add background mask after the transform, to create correct background mask.
+        background_mask = masks.sum(axis=0) == 0
+        target_tensor = torch.cat((masks, background_mask.unsqueeze(0)), dim=0)
+
+        return input_tensor, target_tensor
 
 
 class SegmentationDatamodule(pl.LightningDataModule):
     """
-    Uses the FileDataset to create a LightningDataModule that is used to train the
-    segmentation network. It is a wrapper around the dataset that handles the train
+    Uses the SegmentationFileDataset to create a LightningDataModule that is used to
+    train the segmentation network. It is a wrapper around the dataset that handles the
+    train and validation datasets.
+    """
+
+    def __init__(self, *, batch_size):
+        super().__init__()
+        self.n_workers = min(os.cpu_count(), MAX_CPUS)
+        self.dataset = SegmentationFileDataset()
+
+        self.batch_size = batch_size
+        self.train_set = None
+        self.val_set = None
+
+    def setup(self, stage=None):
+        """
+        This method sets up the training, validating and test datasets. This method is
+        called on every subprocess when initializing the DataLoaders.
+
+        :param stage: stage during which this method was called
+        """
+
+        train_set_size = int(len(self.dataset) * 0.8)
+        valid_set_size = len(self.dataset) - train_set_size
+        self.train_set, self.val_set = random_split(
+            self.dataset, [train_set_size, valid_set_size]
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_set,
+            batch_size=self.batch_size,
+            num_workers=self.n_workers,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_set,
+            batch_size=self.batch_size,
+            num_workers=self.n_workers,
+        )
+
+
+class PresentFileDataset(Dataset):
+    """
+    Extended from the torch.utils.data.Dataset, this class can be used in combination
+    with a Dataloader to lazily load the dataset using files as examples.
+    """
+
+    def __init__(self, dataset_dir, example_names):
+        """
+        :param dataset_dir: str, path to the folder with all examples
+        :param example_names: list of str with all example names
+        """
+        self.train = pd.read_csv(f"{DATA_DIR}/train.csv").pivot(
+            index="id", columns="class", values="segmentation"
+        )
+        self.transform = transforms.Compose(
+            [
+                # Shape (266, 266) because it is most representative of the dataset.
+                transforms.RandomCrop((266, 266), pad_if_needed=True),
+                transforms.RandomRotation(15),
+                transforms.RandomVerticalFlip(p=0.5),
+                transforms.RandomPerspective(distortion_scale=0.1, p=0.1),
+            ]
+        )
+
+    def __len__(self):
+        """
+        :returns: the total number of examples in the dataset
+        """
+        return len(self.train)
+
+    def __getitem__(self, idx):
+        """
+        Given an index, loads the corresponding image and constructs a target tensor.
+        This target tensor will contain a 0 or 1 for every class based on whether it
+        is present in the image or not.
+        We extend the grayscale image to 3 channels such that we can use ResNet.
+
+        :param idx: int, specifying which sample to load
+        :returns: input and target tensors
+        """
+        sample = self.train.iloc[idx]
+        img = get_image_from_id(sample.name)
+        input_tensor = torch.from_numpy(img.reshape([1, *img.shape])).float()
+
+        __import__("pdb").set_trace()
+        large_bowel_present = sample["large_bowel"] != ""
+        small_bowel_present = sample["small_bowel"] != ""
+        stomach_present = sample["stomach"] != ""
+        target_tensor = torch.tensor(
+            [large_bowel_present, small_bowel_present, stomach_present],
+        ).float()
+
+        return self.transform(input_tensor).repeat(3, 1, 1), target_tensor
+
+
+class PresentDatamodule(pl.LightningDataModule):
+    """
+    Uses the PresentFileDataset to create a LightningDataModule that is used to train
+    the present network. It is a wrapper around the dataset that handles the train
     and validation datasets.
     """
 
     def __init__(self, *, batch_size):
         super().__init__()
         self.n_workers = min(os.cpu_count(), MAX_CPUS)
-        self.dataset = FileDataset()
+        self.dataset = PresentFileDataset()
 
         self.batch_size = batch_size
         self.train_set = None

uwm/present_model.py
@@ -1,96 +1,18 @@
 import logging
-import os
 
-import numpy as np
 import pytorch_lightning as pl
 import torch
+import torch.nn.functional as F
 import torchvision
-from constants import PRESENT_DATASET_DIR
-from torch.utils.data import DataLoader, Dataset, random_split
-from torchvision.transforms import CenterCrop
-
-np.set_printoptions(precision=3, suppress=True)
-
-MAX_CPUS = 8
-"""Maximum number of CPU cores to use for loading dataset examples. """
-
-
-class FileDataset(Dataset):
-    """
-    Extended from the torch.utils.data.Dataset, this class can be used in combination
-    with a Dataloader to lazily load the dataset using files as examples.
-    """
-
-    def __init__(self, dataset_dir, example_names):
-        """
-        :param dataset_dir: str, path to the folder with all examples
-        :param example_names: list of str with all example names
-        """
-        self.dataset_dir = dataset_dir
-        self.example_names = example_names
-        self.transform = CenterCrop((224, 224))
-
-    def __len__(self):
-        """
-        :returns: the total number of examples in the dataset
-        """
-        return len(self.example_names)
-
-    def __getitem__(self, idx):
-        """
-        Given an index, queries the database to retrieve data for all sensors
-
-        :param idx: int, specifying which record to load
-        :returns: encoder input, decoder input, targets as tensors
-        """
-        input_tensor, target_tensor = torch.load(
-            os.path.join(self.dataset_dir, self.example_names[idx])
-        )
-
-        return self.transform(input_tensor).repeat(3, 1, 1), target_tensor
-
-
-class PresentDataset(pl.LightningDataModule):
-    def __init__(self, *, batch_size):
-        super().__init__()
-        self.n_workers = min(os.cpu_count(), MAX_CPUS)
-        self.dataset = FileDataset(PRESENT_DATASET_DIR, os.listdir(PRESENT_DATASET_DIR))
-
-        self.batch_size = batch_size
-        self.train_set = None
-        self.val_set = None
-
-    def setup(self, stage=None):
-        """
-        This method sets up the training, validating and test datasets. This method is
-        called on every subprocess when initializing the DataLoaders.
-
-        :param stage: stage during which this method was called
-        """
-
-        train_set_size = int(len(self.dataset) * 0.8)
-        valid_set_size = len(self.dataset) - train_set_size
-        self.train_set, self.val_set = random_split(
-            self.dataset, [train_set_size, valid_set_size]
-        )
-
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_set,
-            batch_size=self.batch_size,
-            num_workers=self.n_workers,
-            shuffle=True,
-        )
-
-    def val_dataloader(self):
-        return DataLoader(
-            self.val_set,
-            batch_size=self.batch_size,
-            num_workers=self.n_workers,
-        )
 
 
 class PresentPredictor(pl.LightningModule):
+    """
+    The PresentPredictor predicts for each class (large_bowel, small_bowel, stomach)
+    whether or not it is present in the given image. It uses a pretrained resnet
+    model, which is finetuned during training.
+    """
+
     def __init__(self):
         super().__init__()
         self.model = torchvision.models.resnet18(pretrained=True)
@@ -103,16 +25,18 @@ class PresentPredictor(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         inputs, targets = batch
         predictions = self.model(inputs)
-        loss = torch.nn.functional.binary_cross_entropy(predictions, targets)
+        loss = F.binary_cross_entropy(predictions, targets)
         return loss
 
     def validation_step(self, batch, batch_idx):
         inputs, targets = batch
         soft_predictions = self.model(inputs)
-        loss = torch.nn.functional.binary_cross_entropy(soft_predictions, targets)
+        loss = F.binary_cross_entropy(soft_predictions, targets)
+
         accuracy, precision, recall = class_wise_metrics(
             soft_predictions.round(), targets
         )
+
         return {
             "loss": loss,
             "accuracy": accuracy,

uwm/segmentation_model.py
@@ -9,9 +9,6 @@ import torch.nn.functional as F
 GRADIENT_CLIP_VALUE = 0.1
 """Clip gradients during training to this number to avoid large gradients. """
 
-PREDICTION_THRESHOLD = 0.5
-""" Probability predictions larger than this value will be predicted as true. """
-
 
 def get_dice_coefficient(preds, targets):
     """
@@ -118,6 +115,7 @@ class SegmentationUNet(pl.LightningModule):
 
         current_channels = start_channels
         self.inc = UNetDoubleConv(n_channels, current_channels)
+        self.softmax = nn.Softmax(dim=1)
 
         # Create the 'down-side layers' of the U
         for i in range(u_depth):
@@ -150,25 +148,26 @@ class SegmentationUNet(pl.LightningModule):
             output_idx = self.u_depth - i - 1
             x = up_layer(x, down_outputs[output_idx])
 
-        return torch.sigmoid(self.conv_out(x))
+        return self.softmax(self.conv_out(x))
 
     def training_step(self, batch, batch_idx):
         inputs, targets = batch
         predictions = self(inputs)
-        # loss = dice_loss(predictions, targets)
-        weights = torch.tensor([1.0, 1.0, 1.0, 0.1], device=self.device)
-        loss = F.cross_entropy(predictions, targets, weights)
+        loss = dice_loss(predictions, targets)
+        # weights = torch.tensor([1.0, 1.0, 1.0, 0.1], device=self.device)
+        # loss = F.cross_entropy(predictions, targets, weights)
         return loss
 
     def validation_step(self, batch, batch_idx):
         inputs, targets = batch
         predictions = self(inputs)
-        # loss = dice_loss(predictions, targets).item()
-        weights = torch.tensor([1.0, 1.0, 1.0, 0.1], device=self.device)
-        loss = F.cross_entropy(predictions, targets, weights).item()
+        loss = dice_loss(predictions, targets).item()
+        # weights = torch.tensor([1.0, 1.0, 1.0, 0.1], device=self.device)
+        # loss = F.cross_entropy(predictions, targets, weights).item()
 
-        predictions[predictions > 0.5] = 1.0
-        predictions[predictions < 0.5] = 0.0
+        max_predictions, _ = torch.max(predictions, dim=1, keepdim=True)
+        predictions[predictions != max_predictions] = 0.0
+        predictions[predictions == max_predictions] = 1.0
         dice_coef = get_multiclass_dice_coefficient(predictions, targets).item()
         return {
             "val_loss": loss,
@@ -183,7 +182,8 @@ class SegmentationUNet(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.99)
+        return [optimizer], [scheduler]
 
 
 class UNetDoubleConv(nn.Module):

uwm/visualization.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.patches import Rectangle
+from uwm.constants import PREDICTION_THRESHOLD
 from uwm.utils import get_image_from_id, get_mask_from_rle
 
 
@@ -68,10 +69,11 @@ def plot_prediction(img, target, pred, alpha=0.9):
     ax2.set_title("Image with true Mask")
     plot_masks(ax2, img, target[0], target[1], target[2])
 
-    pred = np.ma.masked_where(pred < 0.5, pred)
+    pred = np.ma.masked_where(pred < PREDICTION_THRESHOLD, pred)
     ax3.set_title("Image with predicted Mask")
     plot_masks(ax3, img, pred[0], pred[1], pred[2])
 
+    # Show background mask as a vague filter.
     ax3.imshow(pred[3], interpolation="none", alpha=0.4)
 
     plt.show()